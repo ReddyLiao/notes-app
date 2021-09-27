1124e9c5ee0785e3a2de7fe79151b9ff19c9096e
Rumor <rumor.liao@veden.dental>
2021-09-27 15:32:17 +0800

    Integrate selectedNoteId into list item and url

--- imports/routes/routes.js
+++ imports/routes/routes.js
@@ -1,22 +1,22 @@
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
-import { Router, Route, browserHistory } from 'react-router';
+import { Meteor } from "meteor/meteor";
+import React from "react";
+import { Router, Route, browserHistory } from "react-router";
 
-import Signup from '../ui/Signup';
-import Dashboard from '../ui/Dashboard';
-import NotFound from '../ui/NotFound';
-import Login from '../ui/Login';
+import Signup from "../ui/Signup";
+import Dashboard from "../ui/Dashboard";
+import NotFound from "../ui/NotFound";
+import Login from "../ui/Login";
 
-const unauthenticatedPages = ['/', '/signup'];
-const authenticatedPages = ['/dashboard'];
+const unauthenticatedPages = ["/", "/signup"];
+const authenticatedPages = ["/dashboard"];
 const onEnterPublicPage = () => {
   if (Meteor.userId()) {
-    browserHistory.replace('/dashboard');
+    browserHistory.replace("/dashboard");
   }
 };
 const onEnterPrivatePage = () => {
   if (!Meteor.userId()) {
-    browserHistory.replace('/');
+    browserHistory.replace("/");
   }
 };
 export const onAuthChange = (isAuthenticated) => {
@@ -25,16 +25,25 @@ export const onAuthChange = (isAuthenticated) => {
   const isAuthenticatedPage = authenticatedPages.includes(pathname);
 
   if (isUnauthenticatedPage && isAuthenticated) {
-    browserHistory.replace('/dashboard');
+    browserHistory.replace("/dashboard");
   } else if (isAuthenticatedPage && !isAuthenticated) {
-    browserHistory.replace('/');
+    browserHistory.replace("/");
   }
 };
 export const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Login} onEnter={onEnterPublicPage}/>
-    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage}/>
-    <Route path="/dashboard" component={Dashboard} onEnter={onEnterPrivatePage}/>
-    <Route path="*" component={NotFound}/>
+    <Route path="/" component={Login} onEnter={onEnterPublicPage} />
+    <Route path="/signup" component={Signup} onEnter={onEnterPublicPage} />
+    <Route
+      path="/dashboard"
+      component={Dashboard}
+      onEnter={onEnterPrivatePage}
+    />
+    <Route
+      path="/dashboard/:id"
+      component={Dashboard}
+      onEnter={onEnterPrivatePage}
+    />
+    <Route path="*" component={NotFound} />
   </Router>
 );
--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -3,26 +3,43 @@ import React from "react";
 import expect from "expect";
 import { mount } from "enzyme";
 
+import { notes } from "../fixtures/fixtures";
 import NoteListItem from "./NoteListItem";
 
 if (Meteor.isClient) {
   describe("NoteListItem", function () {
+    let Session;
+
+    beforeEach(() => {
+      Session = {
+        set: expect.createSpy(),
+      };
+    });
+
     it("should render title and timestamp", function () {
       const title = "My title here";
       const updateAt = 1632292902966;
-      const wrapper = mount(<NoteListItem note={{ title, updateAt }} />);
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session} />);
 
-      expect(wrapper.find("h5").text()).toBe(title);
+      expect(wrapper.find("h5").text()).toBe(notes[0].title);
       expect(wrapper.find("p").text()).toBe("9/22/21");
     });
 
     it("should set default title if no title set", function () {
-      const title = "";
-      const updateAt = 1632292902966;
-
-      const wrapper = mount(<NoteListItem note={{ title, updateAt }} />);
+      const wrapper = mount(<NoteListItem note={notes[1]} Session={Session} />);
 
       expect(wrapper.find("h5").text()).toBe("Untitle note");
+    });
+
+    it("should call set on click", function () {
+      const wrapper = mount(<NoteListItem note={notes[0]} Session={Session} />);
+
+      wrapper.find("div").simulate("click");
+
+      expect(Session.set).toHaveBeenCalledWith(
+        "notes.selectedNoteIdinsert",
+        notes[0]._id
+      );
     });
   });
 }
